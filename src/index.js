168515257f79a88434ea2ae96d6f2fe1ab0121f0
dzq <x_mitsui@163.com>
2022-03-20 19:56:45 +0800

    添加处理回调的通用方法

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from "./App";
+import App from './App'
 
-import "assets/scss/common.css";
-import "assets/scss/iconfont.css";
-import "assets/scss/buttonStyle.scss";
+import 'assets/scss/common.scss'
+import 'assets/scss/iconfont.css'
+
+import 'assets/scss/buttonStyle.scss'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,5 +1,21 @@
 function trimSpace(str) {
-  return str.replace(/\s+/g, "");
+  return str.replace(/\s+/g, '')
 }
 
-export { trimSpace };
+function getDatas(res, navigate) {
+  const { err_code, err_msg, data } = res
+  return new Promise((resolve, reject) => {
+    if (err_code === 10006) {
+      navigate('/login')
+      reject(err_msg)
+      return
+    }
+    if (err_code === 0 && data) {
+      resolve(data)
+    } else {
+      navigate('/error')
+      reject(err_msg)
+    }
+  })
+}
+export { trimSpace, getDatas }
