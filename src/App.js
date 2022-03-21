0c3181b06cba679e416762895bcaf76e59c781f7
dzq <x_mitsui@163.com>
2022-03-21 12:59:37 +0800

    prettier格式

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import "./assets/scss/index.scss";
+import './assets/scss/index.scss'
 
-import IndexPage from "./pages";
-import LoginPage from "./pages/login";
+import IndexPage from './pages'
+import LoginPage from './pages/login'
 
-import Collection from "./pages/sub/Collection";
-import Course from "./pages/sub/Course";
-import Crawler from "./pages/sub/Crawler";
-import RecomCourse from "./pages/sub/RecomCourse";
-import Slider from "./pages/sub/Slider";
-import Student from "./pages/sub/Student";
-import Teacher from "./pages/sub/Teacher";
+import Collection from './pages/sub/Collection'
+import Course from './pages/sub/Course'
+import Crawler from './pages/sub/Crawler'
+import RecomCourse from './pages/sub/RecomCourse'
+import Slider from './pages/sub/Slider'
+import Student from './pages/sub/Student'
+import Teacher from './pages/sub/Teacher'
 
-import ErrorPage from "./pages/sub/Error";
+import ErrorPage from './pages/sub/Error'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<IndexPage />}>
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<IndexPage />} path="/">
           <Route element={<Course />} path="/course"></Route>
           <Route element={<RecomCourse />} path="/recom_course"></Route>
           <Route element={<Slider />} path="/slider"></Route>
@@ -33,7 +33,7 @@ function App() {
         </Route>
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
