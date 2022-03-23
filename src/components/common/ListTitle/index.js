6fb9f960f2fe849add797d21fcea1f6a7e32e444
dzq <x_mitsui@163.com>
2022-03-23 14:03:34 +0800

    ListTitle对freshbutton的插槽式设计

--- src/components/common/ListTitle/index.js
+++ src/components/common/ListTitle/index.js
@@ -4,14 +4,16 @@ import './index.scss'
 
 export default class ListTitle extends Component {
   render() {
-    const { title, onRefreshData } = this.props
+    const { title, onRefreshData, isFreshShow = true } = this.props
     return (
       <div className="title-wraper">
         <h2 className="name">{title}</h2>
-        <button className="refresh-btn" onClick={onRefreshData}>
-          <i className="iconfont icon-refresh"></i>
-          <span>刷新数据</span>
-        </button>
+        {isFreshShow && (
+          <button className="refresh-btn" onClick={onRefreshData}>
+            <i className="iconfont icon-refresh"></i>
+            <span>刷新数据</span>
+          </button>
+        )}
       </div>
     )
   }
